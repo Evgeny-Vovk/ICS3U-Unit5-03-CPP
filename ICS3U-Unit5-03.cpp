5ec1e94cbbcc4eaa2125ab6721a12441e318a2bd
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-12-07 15:40:34 +0000

    done

--- ICS3U-Unit5-03.cpp
+++ ICS3U-Unit5-03.cpp
@@ -7,48 +7,48 @@
 #include <iostream>
 #include <string>
 
-int convertToMark(std::string(markAsLevel)) {
+int convertToMark(std::string (markAsLevel)) {
     // variables
     int markAsGrade;
 
     // process and output
     if (markAsLevel == "4+") {
         markAsGrade = 97;
-    } else if (markAsGrade == "4") {
+    } else if (markAsLevel == "4") {
         markAsGrade = 90;
-    } else if (markAsGrade == "4-") {
+    } else if (markAsLevel == "4-") {
         markAsGrade = 83;
-    } else if (markAsGrade == "3+") {
+    } else if (markAsLevel == "3+") {
         markAsGrade = 78;
-    } else if (markAsGrade == "3") {
-        markAsGrade = 74;
-    } else if (markAsGrade == "3-") {
+    } else if (markAsLevel == "3") {
+        markAsGrade = 75;
+    } else if (markAsLevel == "3-") {
         markAsGrade = 71;
-    } else if (markAsGrade == "2+") {
+    } else if (markAsLevel == "2+") {
         markAsGrade = 68;
-    } else if (markAsGrade == "2") {
-        markAsGrade = 64;
-    } else if (markAsGrade == "2-") {
+    } else if (markAsLevel == "2") {
+        markAsGrade = 65;
+    } else if (markAsLevel == "2-") {
         markAsGrade = 61;
-    } else if (markAsGrade == "1+") {
+    } else if (markAsLevel == "1+") {
         markAsGrade = 58;
-    } else if (markAsGrade == "1") {
-        markAsGrade = 54;
-    } else if (markAsGrade == "1-") {
+    } else if (markAsLevel == "1") {
+        markAsGrade = 55;
+    } else if (markAsLevel == "1-") {
         markAsGrade = 51;
-    } else if (markAsGrade == "R") {
-        markAsGrade = 30;
-    } else if (markAsGrade == "NE") {
+    } else if (markAsLevel == "R") {
+        markAsGrade = 25;
+    } else if (markAsLevel == "NE") {
         markAsGrade = 0;
     } else {
         markAsGrade = -1;
     }
-    return markAsGrade;
+        return markAsGrade;
 }
 
 int main() {
     // creating variables
-    std::string(markAsLevel);
+    std::string (markAsLevel);
     int markAsGrade;
 
     // input
@@ -57,8 +57,8 @@ int main() {
     std::cout << "\n";
 
     // proccess and output
-    markAsGrade = convertToMark(std::string(markAsGrade));
-    if (markAsGrade <= 100 || markAsGrade >= 0) {
+    markAsGrade = convertToMark(std::string(markAsLevel));
+    if (markAsGrade <= 100 && markAsGrade >= 0) {
         std::cout << "You got a mark of " << markAsGrade << "%.";
     } else if (markAsGrade == -1) {
         std::cout << "Please input a valid mark.";
